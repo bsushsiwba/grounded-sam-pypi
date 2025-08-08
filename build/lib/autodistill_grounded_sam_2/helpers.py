8cef85263a089560456c49faa3c3baa4b8f01247
bsushsiwba <66201863+bsushsiwba@users.noreply.github.com>
2025-08-09 04:06:06 +0500

    Update helpers.py to not use cache directory

--- build/lib/autodistill_grounded_sam_2/helpers.py
+++ build/lib/autodistill_grounded_sam_2/helpers.py
@@ -14,31 +14,14 @@ if not torch.cuda.is_available():
 def load_SAM():
     cur_dir = os.getcwd()
 
-    AUTODISTILL_CACHE_DIR = os.path.expanduser("~/.cache/autodistill")
-    SAM_CACHE_DIR = os.path.join(AUTODISTILL_CACHE_DIR, "segment_anything_2")
+    AUTODISTILL_CACHE_DIR = os.path.expanduser("")
+    SAM_CACHE_DIR = os.path.join(AUTODISTILL_CACHE_DIR)
     SAM_CHECKPOINT_PATH = os.path.join(SAM_CACHE_DIR, "sam2_hiera_base_plus.pth")
 
     url = "https://dl.fbaipublicfiles.com/segment_anything_2/072824/sam2_hiera_base_plus.pt"
 
     # Create the destination directory if it doesn't exist
     os.makedirs(os.path.dirname(SAM_CHECKPOINT_PATH), exist_ok=True)
-
-    os.chdir(SAM_CACHE_DIR)
-
-    if not os.path.exists(os.path.expanduser("~/.cache/autodistill/segment_anything_2/segment-anything-2")):
-        subprocess.run(
-            [
-                "git",
-                "clone",
-                "https://github.com/bsushsiwba/segment-anything-2.git",
-            ]
-        )
-
-        os.chdir("segment-anything-2")
-
-        subprocess.run(["pip", "install", "-e", "."])
-
-    sys.path.append("~/.cache/autodistill/segment_anything_2/segment-anything-2")
 
     # Download the file if it doesn't exist
     if not os.path.isfile(SAM_CHECKPOINT_PATH):
@@ -47,7 +30,7 @@ def load_SAM():
     from sam2.build_sam import build_sam2
     from sam2.sam2_image_predictor import SAM2ImagePredictor
 
-    checkpoint = "~/.cache/autodistill/segment_anything_2/sam2_hiera_base_plus.pth"
+    checkpoint = "sam2_hiera_base_plus.pth"
     checkpoint = os.path.expanduser(checkpoint)
     model_cfg = "sam2_hiera_b+.yaml"
     predictor = SAM2ImagePredictor(build_sam2(model_cfg, checkpoint))
